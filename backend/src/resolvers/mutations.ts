2f35c2363ecccadf14bfbdbb560631ccb83dfb73
Michal <michalszc05@gmail.com>
2023-11-15 19:28:15 +0100

    Fixed tests

--- backend/src/resolvers/mutations.ts
+++ backend/src/resolvers/mutations.ts
@@ -227,7 +227,6 @@ const mutations: MutationResolvers = {
             })
             .catch(err => {
                 logger.error(err);
-                console.error(err); // eslint-disable-line no-console
 
                 return {
                     status: StatusEnum.Error,
@@ -270,7 +269,6 @@ const mutations: MutationResolvers = {
             };
         }).catch(err => {
             logger.error(err);
-            console.error(err); // eslint-disable-line no-console
 
             return {
                 status: StatusEnum.Error,

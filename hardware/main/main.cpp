d55a4a2f0ca445afc7ffb476dcf457cb57b0e03d
Tosiek <antonizieciak@gmail.com>
2023-11-19 13:06:21 +0100

    measuring soil moisture

--- hardware/main/main.cpp
+++ hardware/main/main.cpp
@@ -3,6 +3,7 @@
 #include "diode.h"
 #include "http_request.h"
 #include "nvs_flash.h"
+#include "moisture_sensor.h"
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
@@ -12,9 +13,10 @@ extern "C" void app_main() {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    bool* connected = new bool{false};
-    xTaskCreate(diode::blink_wifi, "blink_connection", configMINIMAL_STACK_SIZE * 3, connected, 5, nullptr);
-    xTaskCreate(http::rq::get_task, "http_request", configMINIMAL_STACK_SIZE * 3, connected, 5, nullptr);
-    ESP_ERROR_CHECK(wifi::wifi_connect(connected));
+    // bool* connected = new bool{false};
+    // xTaskCreate(diode::blink_wifi, "blink_connection", configMINIMAL_STACK_SIZE * 3, connected, 5, nullptr);
+    // xTaskCreate(http::rq::get_task, "http_request", configMINIMAL_STACK_SIZE * 3, connected, 5, nullptr);
+    // ESP_ERROR_CHECK(wifi::wifi_connect(connected));
     // xTaskCreate(dht::dht_test, "dht_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+    xTaskCreate(moisture::measure_moisture_task, "moisture", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
 }

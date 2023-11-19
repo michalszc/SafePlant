6dcc7beecc004379067b9671271117f0bda7586c
Tosiek <antonizieciak@gmail.com>
2023-11-19 13:06:21 +0100

    measuring soil moisture

---
+++ hardware/main/include/moisture_sensor.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_oneshot.h"
+
+namespace moisture {
+    bool init_adc_calibration(adc_unit_t unit,
+                            adc_channel_t channel,
+                            adc_atten_t atten,
+                            adc_cali_handle_t* out_handle);
+
+    void measure_moisture_task(void* params);
+    void measure_moisture(adc_oneshot_unit_handle_t handle);
+}

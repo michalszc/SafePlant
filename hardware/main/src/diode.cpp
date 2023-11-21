272488e89fd93a2faf398c492b111835e64d06ff
Tosiek <antonizieciak@gmail.com>
2023-11-21 17:46:37 +0100

    added unprofessional global variable

--- hardware/main/src/diode.cpp
+++ hardware/main/src/diode.cpp
@@ -1,4 +1,5 @@
 #include "diode.h"
+#include "variables.hpp"
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
@@ -8,12 +9,11 @@
 
 namespace diode {
     void blink_wifi(void* connected) {
-        auto connect = reinterpret_cast<bool*>(connected);
         gpio_reset_pin((gpio_num_t)BLINK_GPIO);
         gpio_set_direction((gpio_num_t)BLINK_GPIO, GPIO_MODE_OUTPUT);
 
         while (true) {
-            if (!*connect) {
+            if (!conn) {
                 bool led_state = true;
                 gpio_set_level((gpio_num_t)BLINK_GPIO, led_state);
                 vTaskDelay(100 / portTICK_PERIOD_MS);
